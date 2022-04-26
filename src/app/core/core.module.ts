18e9db4ca54a91778700998b9ce2c21b70a2e6f9
wixhub <rublin@gmx.de>
2022-04-26 23:20:03 +0200

    feat: add material

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
 import StartPageComponent from './pages/start-page/start-page.component';
 import ErrorPageComponent from './pages/error-page/error-page.component';
 import ContainerComponent from './components/container/container.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 const routes: Routes = [];
 
@@ -16,8 +18,9 @@ const routes: Routes = [];
     ContainerComponent,
     HeaderComponent,
     FooterComponent,
+    LogoComponent,
   ],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
   exports: [RouterModule, HeaderComponent],
 })
-export default class CoreModule {}
+export class CoreModule {}
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [MatIconModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
