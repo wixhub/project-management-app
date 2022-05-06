aa288903bd9f43c6401232aaa31795b445fbfbc8
wixhub <rublin@gmx.de>
2022-05-06 06:45:57 +0200

    fix: removed unused import

--- src/app/main/components/board-view/board-view.component.ts
+++ src/app/main/components/board-view/board-view.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-board-view',
   templateUrl: './board-view.component.html',
   styleUrls: ['./board-view.component.scss'],
 })
-export class BoardViewComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class BoardViewComponent {}
