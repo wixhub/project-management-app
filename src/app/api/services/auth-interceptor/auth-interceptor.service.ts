6772d7f617c950e85892c484c0813ade8e3315d8
valentine909 <valentine.nazarov@gmail.com>
2022-05-06 16:03:23 +0300

    feat: auth interceptor shows error messages

--- src/app/api/services/auth-interceptor/auth-interceptor.service.ts
+++ src/app/api/services/auth-interceptor/auth-interceptor.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
   HttpErrorResponse,
   HttpEvent,
@@ -6,16 +6,19 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import {catchError, Observable, throwError} from 'rxjs';
-import {environment} from "../../../../environments/environment";
-import {AuthenticationService} from '../../../auth/services/authentication/authentication.service';
-import {Router} from "@angular/router";
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { AuthenticationService } from '../../../auth/services/authentication/authentication.service';
+import { Router } from '@angular/router';
+import { ShowMessageService } from '../show-message/show-message.service';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor(private auth: AuthenticationService,
-              private router: Router) {
-  }
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router,
+    private message: ShowMessageService
+  ) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -24,7 +27,10 @@ export class AuthInterceptorService implements HttpInterceptor {
     let newReq = req;
     if (
       req.url.startsWith(environment.endpoint) &&
-      !(req.url.startsWith(environment.endpoint + 'signup') || req.url.startsWith(environment.endpoint + 'signin'))
+      !(
+        req.url.startsWith(environment.endpoint + 'signup') ||
+        req.url.startsWith(environment.endpoint + 'signin')
+      )
     ) {
       newReq = req.clone({
         setHeaders: {
@@ -32,12 +38,16 @@ export class AuthInterceptorService implements HttpInterceptor {
         },
       });
     }
-    return next.handle(newReq).pipe(catchError((errorResponse: HttpErrorResponse) => {
-      if (errorResponse.status === 401) {
-        this.router.navigate(['/login']).then();
-      }
-      return throwError(() => {
-        return new Error(`${errorResponse.message}`);})
-    }));
+    return next.handle(newReq).pipe(
+      catchError((errorResponse: HttpErrorResponse) => {
+        if (errorResponse.status === 401 || errorResponse.status === 403) {
+          this.router.navigate(['/login']).then();
+          this.message.showMessage(`${errorResponse.message}`);
+        }
+        return throwError(() => {
+          return new Error(`${errorResponse.message}`);
+        });
+      })
+    );
   }
 }
