249e79a67b9b83b39dd005ced6229f0357cce30c
valentine909 <valentine.nazarov@gmail.com>
2022-05-06 16:22:11 +0300

    feat: add Auth guard

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication/authentication.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private auth: AuthenticationService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.auth.token) {
+      return true;
+    }
+    this.router
+      .navigate(['/auth'], { queryParams: { returnUrl: state.url } })
+      .then();
+    return false;
+  }
+}
