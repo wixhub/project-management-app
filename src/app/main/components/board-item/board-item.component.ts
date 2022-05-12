7a7fcda374943ff78f9e880d21e3c0d71c4e35e4
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-05-12 18:59:52 +0300

    feat: fix board routing logic 2022-05-12 (18:59:52)

--- src/app/main/components/board-item/board-item.component.ts
+++ src/app/main/components/board-item/board-item.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, Input, OnInit } from '@angular/core';
 import { IBoard } from 'src/app/api/models/APISchemas';
 
@@ -9,7 +10,11 @@ import { IBoard } from 'src/app/api/models/APISchemas';
 export class BoardItemComponent implements OnInit {
   @Input() public item!: IBoard;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  onClickOpenBoard() {
+    this.router.navigate(['main', this.item.id]);
+  }
 }
