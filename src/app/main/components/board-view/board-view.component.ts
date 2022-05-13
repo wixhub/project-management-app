7244738b41482b2ffccb6bfb78d1fe78e72c2e98
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-05-13 13:16:56 +0300

    feat: adjust open board view 2022-05-13 (13:16:55)

--- src/app/main/components/board-view/board-view.component.ts
+++ src/app/main/components/board-view/board-view.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { IColumn, ITask } from './../../../api/models/APISchemas';
+import { DatabaseService } from './../../../api/services/database/database.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-board-view',
   templateUrl: './board-view.component.html',
   styleUrls: ['./board-view.component.scss'],
 })
-export class BoardViewComponent {
-  
+export class BoardViewComponent implements OnInit {
+  @Input() boardId!: string;
+  public boardColumnArr$!: Observable<IColumn[]>;
+  constructor(private databaseService: DatabaseService) {}
+
+  ngOnInit(): void {
+    this.boardColumnArr$ = this.databaseService.getColumns(this.boardId).pipe(
+      map((data) => {
+        if (Array.isArray(data)) {
+          return data;
+        }
+        return [];
+      })
+    );
+  }
+  onClickCreateColumn() {
+    // open modal
+  }
 }
