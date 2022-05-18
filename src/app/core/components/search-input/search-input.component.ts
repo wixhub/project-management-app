61eaf9649cff6c0f375a47f421a005565d8527fb
valentine909 <valentine.nazarov@gmail.com>
2022-05-18 16:11:55 +0300

    feat: search input shows results

--- src/app/core/components/search-input/search-input.component.ts
+++ src/app/core/components/search-input/search-input.component.ts
@@ -1,11 +1,16 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { SearchService } from '../../services/search-service/search.service';
+import {
+  ISearchResults,
+  SearchService,
+} from '../../services/search-service/search.service';
 import {
   debounceTime,
-  distinctUntilChanged,
   filter,
   fromEvent,
   map,
+  merge,
+  combineLatestWith,
+  tap,
 } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { IBoardComplete } from '../../../api/models/APISchemas';
@@ -18,29 +23,80 @@ import { IBoardComplete } from '../../../api/models/APISchemas';
 export class SearchInputComponent implements AfterViewInit {
   @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
 
-  constructor(private search: SearchService) {}
+  isFocused: boolean = false;
+
+  isHovered: boolean = false;
+
+  private readonly defaultValue: ISearchResults;
+
+  results: ISearchResults[];
+
+  constructor(private search: SearchService) {
+    this.defaultValue = {
+      boardId: '',
+      boardName: '',
+      columnName: '',
+      taskName: 'No data found',
+      taskDesc: '',
+    };
+    this.results = [this.defaultValue];
+  }
 
   ngAfterViewInit(): void {
-    let data: IBoardComplete[] = [];
-    fromEvent(this.searchInput.nativeElement, 'focus')
+    const inputEvent$ = fromEvent<KeyboardEvent>(
+      this.searchInput.nativeElement,
+      'input'
+    ).pipe(debounceTime(1500));
+    const enterEvent$ = fromEvent<KeyboardEvent>(
+      this.searchInput.nativeElement,
+      'keyup'
+    ).pipe(filter((event: KeyboardEvent) => event.code === 'Enter'));
+    const data$ = fromEvent(this.searchInput.nativeElement, 'beforeinput').pipe(
+      debounceTime(1000),
+      tap(() => {
+        if (this.searchInput.nativeElement.value.length < 3) {
+          this.results = [this.defaultValue];
+        }
+      }),
+      switchMap(() => {
+        return this.search.getCompleteBoardsData();
+      })
+    );
+    merge(inputEvent$, enterEvent$)
       .pipe(
-        switchMap(() => {
-          return this.search.getCompleteBoardsData();
-        })
+        map<KeyboardEvent, string>(() => this.searchInput.nativeElement.value),
+        filter<string>((searchValue: string) => searchValue.length >= 3),
+        combineLatestWith<string, [IBoardComplete[]]>(data$),
+        map<[string, IBoardComplete[]], ISearchResults[]>(
+          ([searchValue, data]) => this.search.searchInBoards(searchValue, data)
+        )
       )
-      .subscribe((response) => (data = response));
-    fromEvent(this.searchInput.nativeElement, 'input')
-      .pipe(
-        debounceTime(1500),
-        map<Event, string>(
-          (event: Event) => (event.target! as HTMLInputElement).value
-        ),
-        filter((searchValue) => searchValue.length >= 3),
-        distinctUntilChanged(),
-        map((searchKey) => {
-          return this.search.searchInBoards(searchKey, data);
-        })
-      )
-      .subscribe((x) => console.log(x));
+      .subscribe((results) => {
+        results.length === 0
+          ? (this.results = [this.defaultValue])
+          : (this.results = results);
+      });
+  }
+
+  onFocus() {
+    this.isFocused = true;
+  }
+
+  onBlur() {
+    this.isFocused = false;
+  }
+
+  onHover() {
+    this.isHovered = true;
+  }
+
+  onLeave() {
+    this.isHovered = false;
+  }
+
+  onSearch() {
+    if (this.searchInput.nativeElement.value.length === 0) {
+      this.results = [this.defaultValue];
+    }
   }
 }
