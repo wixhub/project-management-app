d693328100fd7b3e57da58cf12878515c5996ccb
Valentine <valentine.nazarov@gmail.com>
2022-05-18 18:54:19 +0300

    refactor: remove log interceptor, remove default login and password

--- src/app/interceptors.ts
+++ src/app/interceptors.ts
@@ -2,7 +2,6 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { BaseUrlInterceptorService } from './api/services/base-url-interceptor/base-url-interceptor.service';
 import { AuthInterceptorService } from './api/services/auth-interceptor/auth-interceptor.service';
-import { LogInterceptorService } from './api/services/log-interceptor/log-interceptor.service';
 
 export const interceptorProviders = [
   {
@@ -11,5 +10,4 @@ export const interceptorProviders = [
     multi: true,
   },
   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: LogInterceptorService, multi: true },
 ];
